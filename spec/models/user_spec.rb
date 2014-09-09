6050b989444cc169e6557b3e736f1eec5025c764
Nathan Hood <nathanhood@me.com>
2014-09-08 19:20:01 -0500

    added email validation
    * uniqueness - ActiveRecord and Adding Unique Index to DB
    * format - regex check, before_save callback that downcases email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,19 +5,59 @@ describe User do
     @user = User.new(name: "Example User", email: "user@example.com")
   end
 
-  context "valid user" do
+  subject { @user }
+
+  context "when valid user information is provided" do
     it "should be a valid user" do
       expect(@user.valid?).to eq(true)
     end
   end
 
-    it "should require a name for a valid user" do
-      @user.name = ""
+  context "when name is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  context "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  context "when name is too long" do
+    before { @user.name = "a" * 51 }
+
+    it "should be invalid" do
       expect(@user.valid?).to eq(false)
+    end
+  end
+
+  context "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  context "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  context "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
     end
 
-    it "should require an email for a valid user" do
-      @user.email = ""
-      expect(@user.valid?).to eq(false)
-    end
+    it { should_not be_valid }
+  end
 end
