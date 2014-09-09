d96b6c7c4179da66004eb59e7be846ddc955c13c
Nathan Hood <nathanhood@me.com>
2014-09-09 11:32:43 -0500

    valid user sign up testing added
    * changed syntax for flash message container in application.html.erb
    * test added - confirmation that redirect goes to user_show page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,7 +24,9 @@ describe "UserPages" do
         before { click_button submit }
 
         it { should have_content('error') }
-        # it { should have_title('Sign up') }
+        pending "Need to trouble shoot why this fails" do
+          it { should have_title('Sign up') }
+        end
       end
     end
 
@@ -35,8 +37,17 @@ describe "UserPages" do
         fill_in "Password", with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
+
       it "should create a user" do
         expect{ click_button submit }.to change(User, :count).by(1)
+      end
+
+      context "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'example@user.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
